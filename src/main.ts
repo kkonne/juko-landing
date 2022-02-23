2311f74d6b8c86469ac9abf3a5a474e162a5599c
Josip <j.leko@globalsoft.ba>
2022-02-23 23:58:47 +0100

    Added new api service to main entry

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-// import axios from 'axios'
-import axios from '@/services/axios'
-import VueAxios from 'vue-axios'
-
-// axios.defaults.baseURL = 'http://192.168.2.29:8000/api/';
-// axios.defaults.headers.common['x-localization'] = `en`
 
 import InlineSvg from 'vue-inline-svg';
 
@@ -16,6 +10,4 @@ const app = createApp(App);
 app.component('inline-svg', InlineSvg);
 app.use(store);
 app.use(router);
-app.use(VueAxios, axios);
-app.provide('axios', app.config.globalProperties.axios);
 app.mount('#app')

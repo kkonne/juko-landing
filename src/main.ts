ae409f4dfcc46ccd42bb730f1a5dceda20790a7c
Josip <j.leko@globalsoft.ba>
2022-03-26 23:28:58 +0100

    Removed old state management

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
 
 import InlineSvg from 'vue-inline-svg';
 
 const app = createApp(App);
 app.component('inline-svg', InlineSvg);
-app.use(store);
 app.use(router);
 app.mount('#app')
